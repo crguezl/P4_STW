00a6a9152c0f53d60845b4f6302256031fd4b2f1
Carlota Lázaro <alu0100698862@ull.edu.es>
2015-10-12 12:39:53 +0000

    Añadiendo mocha, chai y un test

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+var expect = chai.expect;
+
+describe("Temperature", function( {
+    describe("constructor", function() {
+        it("debe tener un valor por defecto", function(){
+            var objeto = new Temperatura();
+            expect(objeto.valor).to.equal("0");
+        });
+        
+        
+}))
