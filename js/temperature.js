62ed20cde424a68ea9d63c1b9800b3486fc2edc9
Carlota Lázaro <alu0100698862@ull.edu.es>
2015-10-12 12:45:02 +0000

    Generando la estructura de la herencia

---
+++ js/temperature.js
@@ -0,0 +1,13 @@
+(function(exports) {
+    "use strict";
+    
+    function Medida(valor,tipo) {
+        
+    }
+    
+    function Temperatura(valor,tipo) {
+        
+    }
+    
+    
+})(this);
